20c304d3517da406a8f23dc0e11f5d329d867adc
ChanoPark <koc081900@korea.ac.kr>
2025-07-10 23:12:34 +0900

    INIT: 프로젝트 생성

---
+++ src/main/kotlin/com/fesi/flowit/FlowitApplication.kt
@@ -0,0 +1,11 @@
+package com.fesi.flowit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowitApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlowitApplication>(*args)
+}
---
+++ src/test/kotlin/com/fesi/flowit/FlowitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fesi.flowit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
