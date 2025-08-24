98ed810631eec0665b9a0692bfd9afd985fc7bf3
sehoon.gim <sehoongim@gmail.com>
2025-08-24 15:13:20 +0900

    REFACTOR: restclient 기반 카카오 api 호출기에 예외 처리 추가한다

--- src/main/kotlin/com/fesi/flowit/auth/social/service/RestClientKakaoApiRequester.kt
+++ src/main/kotlin/com/fesi/flowit/auth/social/service/RestClientKakaoApiRequester.kt
@@ -7,11 +7,10 @@ import com.fesi.flowit.common.logging.loggerFor
 import com.fesi.flowit.common.response.ApiResultCode
 import com.fesi.flowit.common.response.exceptions.AuthException
 import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestClient
-import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
 
 private val log = loggerFor<RestClientKakaoApiRequester>()
 
@@ -19,20 +18,56 @@ private val log = loggerFor<RestClientKakaoApiRequester>()
 class RestClientKakaoApiRequester(
     private val restClient: RestClient
 ) : KakaoApi {
-    override fun requestAccessToken(uri: String, body: KakaoTokenRequestDto): KakaoTokenResponseDto? {
-        return restClient.post()
-            .uri(uri)
-            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            .body(body.toFormData())
-            .retrieve()
-            .body(KakaoTokenResponseDto::class.java)
+    private val CONTENT_TYPE_FORM_UTF8 = "application/x-www-form-urlencoded; charset=UTF-8"
+    private val BEARER_AUTH_TYPE = "Bearer "
+
+    override fun requestAccessToken(
+        uri: String,
+        body: KakaoTokenRequestDto
+    ): KakaoTokenResponseDto? {
+        val formData = body.toFormData()
+        log.debug(">>request access token for kakao service: formData ${formData}")
+
+        return try {
+            restClient.post()
+                .uri(uri)
+                .header(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE_FORM_UTF8)
+                .body(formData)
+                .retrieve()
+                .body(KakaoTokenResponseDto::class.java)
+        } catch (ex: HttpClientErrorException) {
+            val message =
+                "Failed to fetch token from external authentication server. Status: ${ex.statusCode}, Body: ${{ ex.responseBodyAsString }}"
+            log.error(message, ex)
+            throw AuthException.fromCodeWithMsg(ApiResultCode.AUTH_FAIL_TO_FETCH_TOKEN, message)
+        } catch (ex: HttpServerErrorException) {
+            val message =
+                "Failed to fetch token from external authentication server. Status: ${ex.statusCode}, Body: ${{ ex.responseBodyAsString }}"
+            log.error(message, ex)
+            throw AuthException.fromCodeWithMsg(ApiResultCode.AUTH_FAIL_TO_FETCH_TOKEN, message)
+        }
     }
 
     override fun requestUserInfo(uri: String, accessToken: String): KakaoUserInfoResponseDto? {
-        return restClient.get()
-            .uri(uri)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
-            .retrieve()
-            .body(KakaoUserInfoResponseDto::class.java)
+        log.debug(">>request user info for kakao service")
+
+        return try {
+            restClient.get()
+                .uri(uri)
+                .header(HttpHeaders.AUTHORIZATION, BEARER_AUTH_TYPE + accessToken)
+                .header(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE_FORM_UTF8)
+                .retrieve()
+                .body(KakaoUserInfoResponseDto::class.java)
+        } catch (ex: HttpClientErrorException) {
+            val message =
+                "Failed to fetch user info from external authentication server. Status: ${ex.statusCode}, Body: ${ex.responseBodyAsString}"
+            log.error(message, ex)
+            throw AuthException.fromCodeWithMsg(ApiResultCode.AUTH_FAIL_TO_FETCH_USER_INFO, message)
+        } catch (ex: HttpServerErrorException) {
+            val message =
+                "Failed to fetch user info from external authentication server. Status: ${ex.statusCode}, Body: ${ex.responseBodyAsString}"
+            log.error(message, ex)
+            throw AuthException.fromCodeWithMsg(ApiResultCode.AUTH_FAIL_TO_FETCH_USER_INFO, message)
+        }
     }
 }
